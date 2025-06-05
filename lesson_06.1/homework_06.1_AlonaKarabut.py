b3c3c88886661b20476614b29e391517f1bea776
Alona Karabut <alen4ikkarabut@gmail.com>
2025-06-05 22:13:41 +0300

    hometask6.1

--- lesson_06.1/homework_06.1_AlonaKarabut.py
+++ lesson_06.1/homework_06.1_AlonaKarabut.py
@@ -2,12 +2,9 @@
 # Якщо їх більше 10 - вивести в консоль True, інакше - False.
 # Строку отримати за допомогою функції input()
 
-testStringSet = set(input("Enter info: "))
+testString = set(input("Enter info: "))
 
-for uniqueSymbol in testStringSet:
-    if len(testStringSet) > 10:
-        print(True)
-    else:
-        print(False)
-    break
-
+if len(testString) > 10:
+    print(True)
+else:
+    print(False)
